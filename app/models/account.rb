2cd6b8aedfbe6524842c1f6ce6be95a8e4c5ad3b
natalie evans <nmevans7@gmail.com>
2020-06-08 20:16:44 -0600

    added views

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
   belongs_to :user
 
-  validate :balance, numericality:{
+  validates :balance, numericality:{
     greater_than_or_equal_to: 25,
     message: "Must be at least $25"
   }
